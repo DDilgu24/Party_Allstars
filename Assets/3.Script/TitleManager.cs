9e492e6b2976aa513b904632f1e5d23c8f3cb72b
조현준 <chj97610@gmail.com>
2024-08-30 17:59:38 +0900

    [Day 3]

--- Assets/3.Script/TitleManager.cs
+++ Assets/3.Script/TitleManager.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public CanvasGroup canvasGroup; 
+    public float fadeDuration = 1f; // 페이드 시간
+    float elapsedTime = 0f; // 누적 지속시간
+
+    private void Start()
     {
-        
+        StartCoroutine(Fade(true));
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator Fade(bool isFadeIn)
     {
-        
+        elapsedTime = 0f; // 누적 지속시간
+        fadeDuration = (isFadeIn) ? 2f : 1f; // 페이드 시간
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            if(isFadeIn) canvasGroup.alpha = Mathf.Clamp01(elapsedTime / fadeDuration);
+            else canvasGroup.alpha = Mathf.Clamp01(1 - elapsedTime / fadeDuration);
+            yield return null;
+        }
+        canvasGroup.alpha = (isFadeIn)? 1f : 0f; // 최종적으로 완전히 불투명 또는 투명하게 설정
+        if (!isFadeIn) SceneManager.LoadScene("2. Main Menu");
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return) && canvasGroup.alpha == 1f)
+        {
+            StartCoroutine(Fade(false));
+        }
     }
 }
